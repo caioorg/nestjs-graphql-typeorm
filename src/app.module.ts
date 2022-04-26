5677521dfab6aef95728c9f399484c11465f8953
Caio Almeida <caio.almeida@dasa.com.br>
2022-04-25 22:19:21 -0300

    🔧 adding config connection pg, typeorm and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { StudentsModule } from './modules/students/students.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot(),
+    StudentsModule,
+  ],
 })
 export class AppModule {}
